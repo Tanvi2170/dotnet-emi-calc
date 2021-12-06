02d6d91b66c1debf803d5de397a364128dd66fa3
Tanvi Velkar <tanvi.velkar@kilowott.com>
2021-12-06 11:41:35 +0530

    Add project files.

---
+++ Calculator/EmiCal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class EmiCal
+    {
+        internal decimal emi_calculator(decimal p,
+                              decimal r, decimal t, decimal n)
+
+        {
+            decimal emi;
+           r = r / (n * 100); // one month interest
+           t = t * n; // one month period
+            emi =(p * (decimal)Math.Pow(1 +(double)r, (double)t))
+                  / t;
+
+            return (emi);
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+            static void Main(string[] args)
+            { // Driver Program'
+                var spObject = new EmiCal();
+
+            decimal principal, rate, time, emi, n;
+           
+            Console.WriteLine("Enter principal = ");
+            principal = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Rate=");
+            rate = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Enter Time");
+            time = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Enter number of compounding per year = ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+
+
+            emi = spObject.emi_calculator(principal, rate, time, n);
+
+                Console.WriteLine("Monthly EMI is = " + emi);
+                Console.ReadLine();
+
+            }
+        }
+}
